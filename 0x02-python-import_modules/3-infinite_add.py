ec68302225c1a4e07b759bd4d0a64c38004f0ed8
jserumaga <j.serumaga@gmail.com>
2022-12-01 06:57:02 -0800

    3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 
-from sys import argv
-
 if __name__ == "__main__":
-    newsum = 0
-    if len(argv) > 1:
-        for i in range(1, len(argv)):
-            newsum += int(argv[i])
-            print(newsum)
+    import sys
+    argv = sys.argv[1:]
+    argv_count = len(argv)
+    index = 1
+    res = 0
+    while index <= argv_count:
+        res += int(sys.argv[index])
+        index += 1
+        print("{:d}".format(res))
