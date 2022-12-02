6b1222ae385beb52c4e29f2bab3c42345a9bb005
jserumaga <j.serumaga@gmail.com>
2022-12-02 06:24:21 -0800

    lists.h

---
+++ 0x03-python-data_structures/lists.h
@@ -0,0 +1,20 @@
+#ifndef LIST-H
+#define LIST_H
+
+def print_list_integer(my_list=[]):
+def element_at(my_list, idx):
+def replace_in_list(my_list, idx, element):
+def print_reversed_list_integer(my_list=[]):
+def new_in_list(my_list, idx, element):
+def no_c(my_string):
+def print_matrix_integer(matrix=[[]]):
+def add_tuple(tuple_a=(), tuple_b=()):
+def multiple_returns(sentence):
+def max_integer(my_list=[]):
+def divisible_by_2(my_list=[]):
+def delete_at(my_list=[], idx=0):
+int is_palindrome(listint_t **head);
+void print_python_list_info(PyObject *p);
+
+
+#endif /*LIST_H*/
