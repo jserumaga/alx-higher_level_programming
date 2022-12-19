19cb3a89819ebb09bb61fd7d128a22b15a95f6b1
jserumaga <j.serumaga@gmail.com>
2022-12-19 09:42:42 -0800

    File

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+
+def safe_print_integer(value):
+
+    Args:
+        value (int): The integer to print.
+
+    Returns:
+        If a TypeError or ValueError occurs - False
+        Otherwise - True.
+
+    try:
+        print("{:}".format(value))
+        return (True)
+    except (TypeError, ValueError):
+        return (False)
--- 0x05-python-exceptions/103-python.c
+++ 0x05-python-exceptions/103-python.c
@@ -1,1 +1,10 @@
-#include <main.h>
+#include <stdio.h>
+#include <Python.h>
+
+/**
+ * print_python_bytes - Prints bytes information
+ *
+ * @p: Python Object
+ * Return: no return
+ */
+void print_python_bytes(PyObject *p)
