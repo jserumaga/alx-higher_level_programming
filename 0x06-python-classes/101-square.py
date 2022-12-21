3733d9cac8e2c72a392d2960dedb1eed0d0b0731
jserumaga <j.serumaga@gmail.com>
2022-12-21 11:16:39 -0800

    File

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -1,64 +1,55 @@
 #!/usr/bin/python3
 class Square:
-        """ A class that defines a square by its size
-            """
-                def __str__(self):
-                            rtn = ""
-
-                                    if self.size == 0:
-                                                    return rtn
-
-                                                        for i in range(self.position[1]):
-                                                                        rtn += "\n"
-
-                                                                                for i in range(0, self.size):
-                                                                                                for k in range(self.position[0]):
-                                                                                                                    rtn += " "
-                                                                                                                                for j in range(self.size):
-                                                                                                                                                    rtn += "#"
-                                                                                                                                                                if i is not (self.size - 1):
-                                                                                                                                                                                    rtn += "\n"
-
-                                                                                                                                                                                            return rtn
-
-                                                                                                                                                                                            def __init__(self, size=0, position=(0, 0)):
-                                                                                                                                                                                                        """ Method to initialize the square object
-                                                                                                                                                                                                                """
-                                                                                                                                                                                                                        self.size = size
-                                                                                                                                                                                                                                self.position = position
-
-                                                                                                                                                                                                                                    @property
-                                                                                                                                                                                                                                        def size(self):
-                                                                                                                                                                                                                                                    """ Method to returns the size value
-                                                                                                                                                                                                                                                            """
-                                                                                                                                                                                                                                                                    return self.__size
-
-                                                                                                                                                                                                                                                                    @size.setter
-                                                                                                                                                                                                                                                                        def size(self, value):
-                                                                                                                                                                                                                                                                                    """ Method to set the size value of the square object
-                                                                                                                                                                                                                                                                                            """
-                                                                                                                                                                                                                                                                                                    if not isinstance(value, int):
-                                                                                                                                                                                                                                                                                                                    raise TypeError("size must be an integer")
-                                                                                                                                                                                                                                                                                                                        if value < 0:
-                                                                                                                                                                                                                                                                                                                                        raise ValueError("size must be >= 0")
-                                                                                                                                                                                                                                                                                                                                            self.__size = value
-
-                                                                                                                                                                                                                                                                                                                                                @property
-                                                                                                                                                                                                                                                                                                                                                    def position(self):
-                                                                                                                                                                                                                                                                                                                                                                """ Method that returns the position value
-                                                                                                                                                                                                                                                                                                                                                                        """
-                                                                                                                                                                                                                                                                                                                                                                                return self.__position
-
-                                                                                                                                                                                                                                                                                                                                                                                @position.setter
-                                                                                                                                                                                                                                                                                                                                                                                    def position(self, value):
-                                                                                                                                                                                                                                                                                                                                                                                                """ Method that sets the position value of a square object
-                                                                                                                                                                                                                                                                                                                                                                                                        """
-                                                                                                                                                                                                                                                                                                                                                                                                                if not isinstance(value, tuple):
-                                                                                                                                                                                                                                                                                                                                                                                                                                raise TypeError("position must be a tuple of 2 positive integers")
-                                                                                                                                                                                                                                                                                                                                                                                                                                    if len(value) != 2:
-                                                                                                                                                                                                                                                                                                                                                                                                                                                    raise TypeError("position must be a tuple of 2 positive integers")
-                                                                                                                                                                                                                                                                                                                                                                                                                                                        if not isinstance(value[0], int):
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                        raise TypeError("position must be a tuple of 2 positive integers")
+    """ A class that defines a square by its size
+    """
+    def __str__(self):
+        rtn = ""
+        if self.size == 0:
+            return rtn
+        for i in range(self.position[1]):
+            rtn += "\n"
+            for i in range(0, self.size):
+                for k in range(self.position[0]):
+                    rtn += " "
+                    for j in range(self.size):
+                        rtn += "#"
+                        if i is not (self.size - 1):
+                            rtn += "\n"
+                            return rtn
+                        def __init__(self, size=0, position=(0, 0)):
+                        """ Method to initialize the square object
+                        """
+                        self.size = size
+                        self.position = position
+                        @property
+                        def size(self):
+                        """ Method to returns the size value
+                        """
+                        return self.__size
+                    @size.setter
+                    def size(self, value):
+                    """ Method to set the size value of the square object
+                    """
+                    if not isinstance(value, int):
+                        raise TypeError("size must be an integer")
+                    if value < 0:
+                        raise ValueError("size must be >= 0")
+                    self.__size = value
+                    @property
+                    def position(self):
+                        """ Method that returns the position value
+                        """
+                        return self.__position
+                    @position.setter
+                    def position(self, value):
+                        """ Method that sets the position value of a square object
+                        """
+                        if not isinstance(value, tuple):
+                            raise TypeError("position must be a tuple of 2 positive integers")
+                        if len(value) != 2:
+                            raise TypeError("position must be a tuple of 2 positive integers")
+                        if not isinstance(value[0], int):
+                            raise TypeError("position must be a tuple of 2 positive integers")
                                                                                                                                                                                                                                                                                                                                                                                                                                                                             if not isinstance(value[1], int):
                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             raise TypeError("position must be a tuple of 2 positive integers")
                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 if value[0] < 0 or value[1] < 0:
