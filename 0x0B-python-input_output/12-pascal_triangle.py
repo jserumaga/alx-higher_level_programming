86fcb030c876368543f1dbb0c357f74b41bfc693
jserumaga <j.serumaga@gmail.com>
2023-01-10 07:41:14 -0800

    fie

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+"""
+Returns a list of lists of integers
+representing the Pascal’s triangle of n
+"""
+
+
+def pascal_triangle(n):
+    """
+    returns a list of lists of integers
+    representing the Pascal’s triangle of n
+    """
+    if n <= 0:
+        return []
+    res = []
+    l = []
+    for x in range(n):
+        row = []
+        for y in range(x + 1):
+            if x == 0 or y == 0 or x == y:
+                row.append(1)
+            else:
+                row.append(l[y] + l[y - 1])
+                l = row
+                res.append(row)
+                return res
---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,17 @@
+#!/usr/bi/python3
+
+"""
+dds all arguments to a Python list, and then save them to a file
+"""
+import sys
+
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+open("add_item.json", "a")
+try:
+    l = load_from_json_file("add_item.json")
+except ValueError:
+    l = []
+save_to_json_file(l + sys.argv[1:], "add_item.json")
